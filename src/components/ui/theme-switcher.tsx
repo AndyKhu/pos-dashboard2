82308bea337b6099bf616ff1eb1fbab1aa006397
AndyKhu <andykhu02@gmail.com>
2023-09-14 15:23:49 +0700

    fix: hydration error on themeswitcher component

--- src/components/ui/theme-switcher.tsx
+++ src/components/ui/theme-switcher.tsx
@@ -2,9 +2,19 @@
 import { useTheme } from "next-themes";
 import { Button } from "./button";
 import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
 
 const ThemeSwitcher = () => {
+  const [mounted, setMounted] = useState(false)
   const {theme,setTheme} = useTheme()
+   // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
   return (
     <Button size="icon" variant="ghost" onClick={()=>setTheme(theme==="light"?"dark":"light")}>
       {theme==="light"?<Sun size={24}/>:<Moon size={24}/>}
